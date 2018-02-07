62d7282161960234513812481bd7f59ab5f5f347
Makara Csaba <csaba.makara@booom.studio>
2018-02-07 15:34:58 +0100

    feat(containerProvider): add env vars to container

--- src/docker/ContainerProvider.js
+++ src/docker/ContainerProvider.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { runCommand } from '../utils'
+import logger from '../logger'
 
 export default async (...args) => {
   const container = new Container(...args)
@@ -12,6 +13,12 @@ class Container {
   constructor (imageUrl, options) {
     this.imageUrl = imageUrl
     this.volumes = options ? options.volumes : []
+    this.environment = {}
+  }
+
+  addEnvironmentVariable (name, value) {
+    logger.debug(`Adding environment variable ${name} to container`)
+    this.environment[name] = value
   }
 
   async initializeContainer (imageUrl) {
@@ -19,9 +26,15 @@ class Container {
   }
 
   async run (command) {
-    return runCommand(`docker run \
-      ${this.volumes.length ? '-v ' : ''}${this.volumes.join('-v ')} \
-      ${this.imageUrl} \
-      ${command}`)
+    var environmentString = ''
+    for (let key of Object.keys(this.environment)) {
+      environmentString += ` -e ${key}=${this.environment[key]}`
+    }
+    var commandToRun = 'docker run '
+    commandToRun += environmentString
+    commandToRun += ` ${this.volumes.length ? '-v ' : ''}${this.volumes.join('-v ')}`
+    commandToRun += this.imageUrl
+    commandToRun += ` ${command}`
+    return runCommand(commandToRun)
   }
 }
